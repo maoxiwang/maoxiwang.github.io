7f6029ebd0fb64542818577b2c515dce14790dcc
George <maoxiwang@gwu.edu>
2020-08-30 18:19:30 -0700

    Updates

---
+++ precache-manifest.e42b6ebd0c327c5c449c32a663657f2b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3539e55b6c643541c7b74baa28f2f5b3",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "2b7aa967ffe15bf48144",
+    "url": "/website/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c5bc45f160b7f997c13a",
+    "url": "/website/static/css/main.36d3dee8.chunk.css"
+  },
+  {
+    "revision": "2b7aa967ffe15bf48144",
+    "url": "/website/static/js/2.2fee0f43.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/website/static/js/2.2fee0f43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5bc45f160b7f997c13a",
+    "url": "/website/static/js/main.2decc4b7.chunk.js"
+  },
+  {
+    "revision": "9c23e00f99dd7916e537",
+    "url": "/website/static/js/runtime-main.9b825fe6.js"
+  },
+  {
+    "revision": "ee0a701c81ae3d60af6243f0f59b8876",
+    "url": "/website/static/media/BattleBot_image.ee0a701c.png"
+  },
+  {
+    "revision": "046fb13b4738610e6291f3956e24d317",
+    "url": "/website/static/media/FEA_Shaft.046fb13b.png"
+  },
+  {
+    "revision": "6de930eb65c4fbed3d095cc68d4e7993",
+    "url": "/website/static/media/Speaker.6de930eb.jpeg"
+  }
+]);
