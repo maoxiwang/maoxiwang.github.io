a38f4f639c32fbd93e069a4abc1b004ab75ea278
Zhaohong wang <zwang@Zhaohongs-iMac.local>
2021-01-07 22:26:18 -0800

    Update

---
+++ src/pages/ANOVA.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const BattleBots = () => (
+  <Container>
+    <Row>
+      <Col>
+        ANOVA
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default BattleBots;
---
+++ src/pages/Speaker.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+const BattleBots = () => (
+  <Container>
+    <Row>
+      <Col>
+        Speaker
+      </Col>
+    </Row>
+  </Container>
+);
+
+export default BattleBots;
